cbe8855f897c134523ceea0baafbc9ce25d5f289
EliezerAmbion <eliezerambion@gmail.com>
2023-04-01 14:05:10 +0800

    add logics to main for future use

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,99 @@
 import 'package:flutter/material.dart';
 
 import 'config/theme.dart';
+import 'dummy_data.dart';
+import 'models/meal.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+// import 'screens/filters_screen.dart';
+// import 'screens/meal_detail_screen.dart';
+// import 'screens/category_meals_screen.dart';
+// import 'screens/tabs_screen.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: theme(),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  State<MyApp> createState() => _MyAppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class _MyAppState extends State<MyApp> {
+  Map<String, bool> _filters = {
+    'isGlutenFree': false,
+    'isLactoseFree': false,
+    'isVegan': false,
+    'isVegetarian': false,
+  };
 
-  final String title;
+  List<Meal> _availableMeals = DUMMY_MEALS;
+  List<Meal> _favoriteMeals = [];
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  void _setFilters(final Map<String, bool> filterData) {
     setState(() {
-      _counter++;
+      _filters = filterData;
+
+      _availableMeals = DUMMY_MEALS.where((meal) {
+        if (_filters['isGlutenFree']! && !meal.isGlutenFree) {
+          return false;
+        }
+        if (_filters['isLactoseFree']! && !meal.isLactoseFree) {
+          return false;
+        }
+        if (_filters['isVegan']! && !meal.isVegan) {
+          return false;
+        }
+        if (_filters['isVegetarian']! && !meal.isVegetarian) {
+          return false;
+        }
+
+        return true;
+      }).toList();
     });
   }
 
+  void _toggleFavorite(String mealId) {
+    final existingIndex =
+        _favoriteMeals.indexWhere((meal) => meal.id == mealId);
+
+    if (existingIndex >= 0) {
+      setState(() {
+        _favoriteMeals.removeAt(existingIndex);
+      });
+    } else {
+      setState(() {
+        _favoriteMeals.add(
+          DUMMY_MEALS.firstWhere((meal) => meal.id == mealId),
+        );
+      });
+    }
+  }
+
+  bool _isMealFavorite(String id) {
+    return _favoriteMeals.any((meal) => meal.id == id);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'DeliMeals',
+      theme: theme(),
+
+      // routes: {
+      //   '/': (ctx) => TabsScreen(
+      //         favoriteMeals: _favoriteMeals,
+      //       ),
+      //   CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(
+      //         availableMeals: _availableMeals,
+      //       ),
+      //   MealDetailScreen.routeName: (ctx) => MealDetailScreen(
+      //         toggleFavorite: _toggleFavorite,
+      //         isMealFavorite: _isMealFavorite,
+      //       ),
+      //   FiltersScreen.routeName: (ctx) => FiltersScreen(
+      //         saveFilters: _setFilters,
+      //         currentFilters: _filters,
+      //       ),
+      // },
     );
   }
 }
---
+++ lib/widgets/bottom_nav_bar.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatelessWidget {
+  final void Function(int) selectPage;
+  final int selectedPageIndex;
+
+  const BottomNavBar({
+    super.key,
+    required this.selectPage,
+    required this.selectedPageIndex,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      onTap: selectPage,
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      unselectedItemColor: Colors.white,
+      selectedItemColor: Theme.of(context).colorScheme.tertiary,
+      currentIndex: selectedPageIndex,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.category),
+          label: 'Categories',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.star),
+          label: 'Favourites',
+        ),
+      ],
+    );
+  }
+}
